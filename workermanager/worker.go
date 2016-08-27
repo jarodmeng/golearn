e53e64b2524779490d87d566d0b7c9c8dcceb95f
Jarod G.R. Meng <genrong.meng@gmail.com>
2016-08-27 16:36:57 -0700

    Modify input and output from channel to slice

--- workermanager/worker.go
+++ workermanager/worker.go
@@ -29,16 +29,24 @@ type worker struct {
 	workQueue chan *work
 }
 
-func newManager(n int, in chan *work) *manager {
+func newManager(n int, in []*work) *manager {
 	if n <= 0 {
 		log.Fatalln("# of workers cannot be less than 1.")
 	}
+
+	c := make(chan *work)
+	go func() {
+		for _, wk := range in {
+			c <- wk
+		}
+		close(c)
+	}()
 
 	m := &manager{
 		nWorkers: n,
 		team:     make(chan *worker, n),
 		finish:   &sync.WaitGroup{},
-		input:    in,
+		input:    c,
 		output:   make(chan *result),
 	}
 
@@ -68,7 +76,7 @@ func processWork(in *work) (out *result, err error) {
 	return
 }
 
-func (m *manager) work() chan *result {
+func (m *manager) work() []*result {
 	for i := 0; i < m.nWorkers; i++ {
 		w := m.newWorker(i)
 		w.work()
@@ -95,7 +103,12 @@ func (m *manager) work() chan *result {
 		close(m.team)
 	}()
 
-	return m.output
+	out := make([]*result, 0)
+	for r := range m.output {
+		out = append(out, r)
+	}
+
+	return out
 }
 
 func (w *worker) work() {
@@ -117,20 +130,16 @@ func (w *worker) work() {
 
 func main() {
 	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	input := make(chan *work)
-
-	go func() {
-		for _, n := range nums {
-			input <- &work{num: n}
-		}
-		// fmt.Println("Closing input.")
-		close(input)
-	}()
+	input := make([]*work, 0)
+	for _, n := range nums {
+		input = append(input, &work{num: n})
+	}
 
 	m := newManager(1, input)
 	out := m.work()
 
-	for r := range out {
+	fmt.Printf("Output is a %d-element slice.\n", len(out))
+	for _, r := range out {
 		fmt.Println(r.num)
 	}
 }
