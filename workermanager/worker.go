dd6fa5548abc06b829882c5b233fde373d35f4af
Jarod G.R. Meng <genrong.meng@gmail.com>
2016-08-27 20:59:54 -0700

    Add retry support

--- workermanager/worker.go
+++ workermanager/worker.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"math"
 	"sync"
 )
 
@@ -15,12 +16,18 @@ type result struct {
 	num int
 }
 
+type fail struct {
+	sync.RWMutex
+	fail []*work
+}
+
 type manager struct {
 	nWorkers int
 	team     chan *worker
 	finish   *sync.WaitGroup
 	input    <-chan *work
 	output   chan *result
+	fail     fail
 }
 
 type worker struct {
@@ -48,6 +55,7 @@ func newManager(n int, in []*work) *manager {
 		finish:   &sync.WaitGroup{},
 		input:    c,
 		output:   make(chan *result),
+		fail:     fail{fail: make([]*work, 0)},
 	}
 
 	return m
@@ -69,14 +77,14 @@ func processWork(in *work) (out *result, err error) {
 			err = r.(error)
 		}
 	}()
-	if in.num == 5 {
+	if math.Mod(float64(in.num), 5.0) == 0.0 {
 		panic(errors.New("It's a 5!"))
 	}
 	out = &result{num: in.num * in.num}
 	return
 }
 
-func (m *manager) work() []*result {
+func (m *manager) work() ([]*result, []*work) {
 	for i := 0; i < m.nWorkers; i++ {
 		w := m.newWorker(i)
 		w.work()
@@ -108,7 +116,7 @@ func (m *manager) work() []*result {
 		out = append(out, r)
 	}
 
-	return out
+	return out, m.fail.fail
 }
 
 func (w *worker) work() {
@@ -118,6 +126,9 @@ func (w *worker) work() {
 			r, err := processWork(wk)
 			if err != nil {
 				fmt.Printf("Unable to process work: %v.\n", err)
+				w.manager.fail.Lock()
+				w.manager.fail.fail = append(w.manager.fail.fail, wk)
+				w.manager.fail.Unlock()
 			} else {
 				w.manager.output <- r
 			}
@@ -135,11 +146,30 @@ func main() {
 		input = append(input, &work{num: n})
 	}
 
-	m := newManager(1, input)
-	out := m.work()
+	m := newManager(2, input)
+	out, fail := m.work()
+	if len(fail) > 0 {
+		for i := 0; i < 3; i++ {
+			if len(fail) == 0 {
+				break
+			}
+
+			m = newManager(2, fail)
+			a, b := m.work()
+			if len(a) > 0 {
+				out = append(out, a...)
+			}
+			fail = b
+		}
+	}
 
 	fmt.Printf("Output is a %d-element slice.\n", len(out))
 	for _, r := range out {
 		fmt.Println(r.num)
+	}
+
+	fmt.Printf("Fail is a %d-element slice.\n", len(fail))
+	for _, f := range fail {
+		fmt.Println(f.num)
 	}
 }
